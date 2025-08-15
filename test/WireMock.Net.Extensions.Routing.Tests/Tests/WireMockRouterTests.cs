25f262a0bdc9764600878f7d6254a6af212422e6
Gennadii Saltyshchak <GennadyGS@gmail.com>
2025-08-15 12:42:23 +0300

    Migrate to AwesomeAssertions

--- test/WireMock.Net.Extensions.Routing.Tests/Tests/WireMockRouterTests.cs
+++ test/WireMock.Net.Extensions.Routing.Tests/Tests/WireMockRouterTests.cs
@@ -3,7 +3,7 @@
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
-using FluentAssertions;
+using AwesomeAssertions;
 using WireMock.Net.Extensions.Routing.Extensions;
 using WireMock.Server;
 
