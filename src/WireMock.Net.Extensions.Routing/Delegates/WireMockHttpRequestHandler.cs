c523a116b143ca06c434deb9d88f57de209d52d1
Gennadii Saltyshchak <GennadyGS@gmail.com>
2025-08-15 13:56:03 +0300

    Refactoring: rename delegate parameter

--- src/WireMock.Net.Extensions.Routing/Delegates/WireMockHttpRequestHandler.cs
+++ src/WireMock.Net.Extensions.Routing/Delegates/WireMockHttpRequestHandler.cs
@@ -5,6 +5,6 @@ namespace WireMock.Net.Extensions.Routing.Delegates;
 /// <summary>
 /// Represents a handler for processing WireMock HTTP requests and returning a response asynchronously.
 /// </summary>
-/// <param name="requests">The incoming request message.</param>
+/// <param name="requestMessage">The incoming request message.</param>
 /// <returns>A task that resolves to a <see cref="ResponseMessage"/>.</returns>
-public delegate Task<ResponseMessage> WireMockHttpRequestHandler(IRequestMessage requests);
+public delegate Task<ResponseMessage> WireMockHttpRequestHandler(IRequestMessage requestMessage);
