5f6f5663e174ac877528c33d27a67511988bc8e6
Stef Heyenrath <stef.heyenrath@gmail.com>
2026-01-02 21:37:07 +0100

    mm

--- test/WireMock.Net.Tests/WireMockServerTests.WithBody.cs
+++ test/WireMock.Net.Tests/WireMockServerTests.WithBody.cs
@@ -183,6 +183,7 @@ public partial class WireMockServerTests
                       {"jsonrpc":"2.0","id":"{{request.bodyAsJson.id}}","result":{"protocolVersion":"2024-11-05","capabilities":{"logging":{},"prompts":{"listChanged":true},"resources":{"subscribe":true,"listChanged":true},"tools":{"listChanged":true}},"serverInfo":{"name":"ExampleServer","version":"1.0.0"}}}
                       """)
             .WithStatusCode(200)
+            .WithTransformer(true)
         );
 
         // Act
