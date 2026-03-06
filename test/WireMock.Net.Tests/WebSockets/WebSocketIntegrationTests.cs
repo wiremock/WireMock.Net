07d3c6ad51fa9e030848895d4f3952a7006081aa
Stef Heyenrath <stef.heyenrath@gmail.com>
2026-03-06 19:22:46 +0100

    500

--- test/WireMock.Net.Tests/WebSockets/WebSocketIntegrationTests.cs
+++ test/WireMock.Net.Tests/WebSockets/WebSocketIntegrationTests.cs
@@ -796,7 +796,7 @@ public class WebSocketIntegrationTests(ITestOutputHelper output, ITestContextAcc
         var proxyUri = new Uri($"{sut.Url}/ws/proxy");
         await client.ConnectAsync(proxyUri, _ct);
 
-        await Task.Delay(250, _ct);
+        await Task.Delay(500, _ct);
 
         var testData = new byte[] { 0x01, 0x02, 0x03, 0x04, 0x05 };
 
@@ -805,7 +805,7 @@ public class WebSocketIntegrationTests(ITestOutputHelper output, ITestContextAcc
 
         var receivedData = await client.ReceiveAsBytesAsync(cancellationToken: _ct);
 
-        await Task.Delay(250, _ct);
+        await Task.Delay(500, _ct);
 
         // Assert
         receivedData.Should().BeEquivalentTo(testData, "binary data should be proxied and echoed back");
