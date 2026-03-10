e18de41da5b1995bdfa9a5134f6d16b684be0f5c
m4tchl0ck <m4tchl0ck@outlook.com>
2026-03-10 21:45:15 +0000

    Move ScenarioState to Abstractions and add IScenarioStateStore interface
    ScenarioState is moved to the Abstractions project so it can be referenced
    by the new IScenarioStateStore interface. The interface defines the contract
    for storing and retrieving scenario states, enabling distributed implementations.

---
+++ src/WireMock.Net.Abstractions/Handlers/IScenarioStateStore.cs
@@ -0,0 +1,26 @@
+// Copyright © WireMock.Net
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace WireMock.Handlers;
+
+public interface IScenarioStateStore
+{
+    bool TryGet(string name, [NotNullWhen(true)] out ScenarioState? state);
+
+    IReadOnlyList<ScenarioState> GetAll();
+
+    bool ContainsKey(string name);
+
+    bool TryAdd(string name, ScenarioState scenarioState);
+
+    ScenarioState AddOrUpdate(string name, Func<string, ScenarioState> addFactory, Func<string, ScenarioState, ScenarioState> updateFactory);
+
+    ScenarioState? Update(string name, Action<ScenarioState> updateAction);
+
+    bool TryRemove(string name);
+
+    void Clear();
+}
