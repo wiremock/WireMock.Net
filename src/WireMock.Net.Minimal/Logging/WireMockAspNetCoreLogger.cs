f1ac7b3774b1fc34c1f6a18e014dbd51c7dd83c7
Stef Heyenrath <stef.heyenrath@gmail.com>
2026-03-14 10:02:52 +0100

    x

--- src/WireMock.Net.Minimal/Logging/WireMockAspNetCoreLogger.cs
+++ src/WireMock.Net.Minimal/Logging/WireMockAspNetCoreLogger.cs
@@ -4,17 +4,8 @@ using Microsoft.Extensions.Logging;
 
 namespace WireMock.Logging;
 
-internal sealed class WireMockAspNetCoreLogger : ILogger
+internal sealed class WireMockAspNetCoreLogger(IWireMockLogger logger, string categoryName) : ILogger
 {
-    private readonly IWireMockLogger _logger;
-    private readonly string _categoryName;
-
-    public WireMockAspNetCoreLogger(IWireMockLogger logger, string categoryName)
-    {
-        _logger = logger;
-        _categoryName = categoryName;
-    }
-
     public IDisposable? BeginScope<TState>(TState state) where TState : notnull => null;
 
     public bool IsEnabled(LogLevel logLevel) => logLevel >= LogLevel.Warning;
@@ -36,11 +27,11 @@ internal sealed class WireMockAspNetCoreLogger : ILogger
         switch (logLevel)
         {
             case LogLevel.Warning:
-                _logger.Warn("[{0}] {1}", _categoryName, message);
+                logger.Warn("[{0}] {1}", categoryName, message);
                 break;
 
             default:
-                _logger.Error("[{0}] {1}", _categoryName, message);
+                logger.Error("[{0}] {1}", categoryName, message);
                 break;
         }
     }
