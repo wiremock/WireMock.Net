f2b03cfe0fcad35a61910ca910e69cef0566b45a
Stef Heyenrath <stef.heyenrath@gmail.com>
2026-03-30 19:09:57 +0200

    Add comments for ScenarioStateStore related code

--- src/WireMock.Net.Abstractions/Handlers/IScenarioStateStore.cs
+++ src/WireMock.Net.Abstractions/Handlers/IScenarioStateStore.cs
@@ -1,7 +1,5 @@
 // Copyright © WireMock.Net
 
-using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 
 namespace WireMock.Handlers;
--- src/WireMock.Net.Shared/Handlers/InMemoryScenarioStateStore.cs
+++ src/WireMock.Net.Shared/Handlers/InMemoryScenarioStateStore.cs
@@ -1,42 +1,48 @@
 // Copyright © WireMock.Net
 
-using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 
 namespace WireMock.Handlers;
 
+/// <summary>
+/// Provides an in-memory implementation of the <see cref="IScenarioStateStore" /> interface for managing scenario state objects by name.
+/// </summary>
 public class InMemoryScenarioStateStore : IScenarioStateStore
 {
     private readonly ConcurrentDictionary<string, ScenarioState> _scenarios = new(StringComparer.OrdinalIgnoreCase);
 
+    /// <inheritdoc />
     public bool TryGet(string name, [NotNullWhen(true)] out ScenarioState? state)
     {
         return _scenarios.TryGetValue(name, out state);
     }
 
+    /// <inheritdoc />
     public IReadOnlyList<ScenarioState> GetAll()
     {
         return _scenarios.Values.ToArray();
     }
 
+    /// <inheritdoc />
     public bool ContainsKey(string name)
     {
         return _scenarios.ContainsKey(name);
     }
 
+    /// <inheritdoc />
     public bool TryAdd(string name, ScenarioState scenarioState)
     {
         return _scenarios.TryAdd(name, scenarioState);
     }
 
+    /// <inheritdoc />
     public ScenarioState AddOrUpdate(string name, Func<string, ScenarioState> addFactory, Func<string, ScenarioState, ScenarioState> updateFactory)
     {
         return _scenarios.AddOrUpdate(name, addFactory, updateFactory);
     }
 
+    /// <inheritdoc />
     public ScenarioState? Update(string name, Action<ScenarioState> updateAction)
     {
         if (_scenarios.TryGetValue(name, out var state))
@@ -48,11 +54,13 @@ public class InMemoryScenarioStateStore : IScenarioStateStore
         return null;
     }
 
+    /// <inheritdoc />
     public bool TryRemove(string name)
     {
         return _scenarios.TryRemove(name, out _);
     }
 
+    /// <inheritdoc />
     public void Clear()
     {
         _scenarios.Clear();
